d0849471edd6e98dd6340fc45b30402712bd6360
RIkhardson <n2puri00@students.oamk.fi>
2022-11-10 13:08:56 +0200

    Updated page, style and javascript added

---
+++ code.js
@@ -0,0 +1,7 @@
+function changeColor(){
+    let elems = document.querySelectorAll("ul:first-child li");
+
+    for (constelem of elems) {
+        elem.classList.toggle("colored");
+    }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+Tämä on rikun hieno projekti
